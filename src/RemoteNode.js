c2f32458be2bfc1feba7ef76721822cedd622831
brambilla <brambilla@users.noreply.github.com>
2016-04-19 00:06:00 +0200

    Update RemoteNode.js

--- src/RemoteNode.js
+++ src/RemoteNode.js
@@ -1,83 +1,84 @@
 class RemoteNode {
-	
-	constructor(peer, descriptor, options, bootstrappingNode, signalingNode) {
-		this.peer = peer;
-		this.descriptor = descriptor;
-		this.options = options;
-		this.bootstrappingNode = bootstrappingNode;
-		this.signalingNode = signalingNode;
-		
-		var self = this;
-		
-		this.connection = new RTCPeerConnection(options.configuration);
-		this.datachannel = this.connection.createDataChannel(options.label, options.dataChannelDict);
 
-		this.connection.ondatachannel = function(event) {
-			event.channel.onmessage = function(event) {
-				self.peer.onmessage(JSON.parse(event.data), self);
-			};
-				
-			event.channel.onopen = function (event) {
-				self.peer.emit("open", event);
-			};
-			
-			event.channel.onclose = function (event) {
-				self.peer.geobucket.remove(self);
-				self.peer.emit("close", event);
-			};
-			
-			event.channel.onerror = function (error) {
-				self.peer.emit("error", event);
-			};
-		};
-		
-		this.connection.onicecandidate = function (event) {
-			if (event.candidate) {
-				self.sendToSignalingNode(new SignalingMessage(self.peer.descriptor, self.descriptor, new CandidateMessage(self.peer.descriptor, event.candidate)));
-			}
-		};
-	}
-	
-	isConnected() {
-		return this.datachannel.readyState === "open";
-	}
+    constructor(peer, descriptor, options, bootstrappingNode, signalingNode, isCalled) {
+        this.peer = peer;
+        this.descriptor = descriptor;
+        this.options = options;
+        this.bootstrappingNode = bootstrappingNode;
+        this.signalingNode = signalingNode;
 
-	connect() {
-		var self = this;
-		this.connection.createOffer(function (offer) {
-			self.connection.setLocalDescription(new RTCSessionDescription(offer), function() {
-				self.sendToSignalingNode(new SignalingMessage(self.peer.descriptor, self.descriptor, new OfferMessage(self.peer.descriptor, offer)));
-			}, function (error) {
-				self.peer.emit("error", error);
-			});
-		}, function (error) {
-			self.peer.emit("error", error);
-		});
-	}
+        var self = this;
+        this.connection = new RTCPeerConnection(options.configuration);
 
-	send(message) {
-		this.datachannel.send(JSON.stringify(message));
-	}
+        this.connection.onicecandidate = function (event) {
+            if(event.candidate) {
+                self.sendToSignalingNode(new SignalingMessage(self.peer.descriptor, self.descriptor, new CandidateMessage(self.peer.descriptor, event.candidate)));
+            }
+        };
 
-	update(descriptor) {
-		this.descriptor = descriptor;
-	}
+        this.connection.ondatachannel = function(event) {
+            self.datachannel = event.channel;
 
-	sendToSignalingNode(message) {
-		if(this.signalingNode.isConnected()) {
-			this.signalingNode.send(message);
-		} else {
-			this.bootstrappingNode.send(message);
-		}
-	}
-		
-	disconnect() {
-		if(this.isConnected()) {
-			this.sendToSignalingNode(new SignalingMessage(this.peer.descriptor, this.descriptor, new LeaveMessage(this.peer.descriptor)));
-			this.datachannel.close();
-			this.connection.close();
-			this.connection.onicecandidate = null;
-		}
-	}
+            self.datachannel.onmessage = function(event) {
+                self.peer.onmessage(JSON.parse(event.data), self);
+            };
+
+            self.datachannel.onclose = function (event) {
+                self.peer.geobucket.remove(self);
+                self.peer.emit("neighbors", self.peer.geobucket.descriptors());
+            };
+        };
+
+        if(isCalled) {
+            this.datachannel = this.connection.createDataChannel(options.label, options.datachannel_options);
+
+            this.datachannel.onmessage = function(event) {
+                self.peer.onmessage(JSON.parse(event.data), self);
+            };
+
+            this.datachannel.onclose = function (event) {
+                self.peer.geobucket.remove(self);
+                self.peer.emit("neighbors", self.peer.geobucket.descriptors());
+            };
+        }
+    }
+
+    isConnected() {
+        return this.datachannel.readyState === "open";
+    }
+
+    connect() {
+        var self = this;
+
+        this.connection.createOffer().then(function(offer) {
+            return self.connection.setLocalDescription(offer);
+        }).then(function() {
+            self.sendToSignalingNode(new SignalingMessage(self.peer.descriptor, self.descriptor, new OfferMessage(self.peer.descriptor, self.connection.localDescription)));
+        }).catch(function(reason) {
+            // An error occurred, so handle the failure to connect
+        });
+    }
+
+    send(message) {
+        this.datachannel.send(JSON.stringify(message));
+        console.log('Sent ' + message.type + ' to ' + this.descriptor.key);
+    }
+
+    sendToSignalingNode(message) {
+        if(this.signalingNode.isConnected()) {
+            this.signalingNode.send(message);
+        } else {
+            this.bootstrappingNode.send(message);
+        }
+    }
+
+    disconnect() {
+        if(this.isConnected()) {
+            this.sendToSignalingNode(new SignalingMessage(this.peer.descriptor, this.descriptor, new LeaveMessage(this.peer.descriptor)));
+            this.datachannel.close();
+            this.connection.close();
+            this.connection.onicecandidate = null;
+        }
+    }
 
 }
